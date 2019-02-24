9f057444699544e6583d1ed1e1e116ff385541de
y <benyasminenl@gmail.com>
2019-02-24 16:00:28 +0100

    Added LinkedList search
    LinkedlistSortcomplete

--- src/main/java/nl/hva/ict/ss/util/LinkedListSort.java
+++ src/main/java/nl/hva/ict/ss/util/LinkedListSort.java
@@ -1,4 +1,48 @@
 package nl.hva.ict.ss.util;
 
+import java.util.LinkedList;
+
 public class LinkedListSort {
+
+    public static <E extends Comparable<E>> LinkedList<E> LinkedListSorted(LinkedList<E> sortingList) {
+
+        if (1 == sortingList.size()) {
+            return sortingList;
+        }
+        else if(sortingList.size() == 0 || sortingList == null){
+            return sortingList;
+        }
+        else {
+            LinkedList LinkedListReturn = new LinkedList();
+
+            E pivot = sortingList.getFirst();
+            LinkedList<E> highBound = new LinkedList<>();
+            LinkedList<E> lowBound = new LinkedList<>();
+                while (sortingList.size() != 1){
+                    E comparedObject = sortingList.pollLast();
+                    if( 0 <= pivot.compareTo(comparedObject)){
+                        lowBound.add(comparedObject);
+                    }
+                    else{
+                        highBound.add(comparedObject);
+                    }
+
+                }
+
+                // TODO: 2/24/2019  als dit een error veroorzaakt moet dit en lowbound gebeuren geswapt worden.
+            highBound = LinkedListSorted(highBound);
+            while (0 < highBound.size()){
+                LinkedListReturn.add(highBound.pollFirst());
+
+            }
+            lowBound = LinkedListSorted(lowBound);
+            while (0 != lowBound.size()){
+                LinkedListReturn.add(lowBound.pollFirst());
+            }
+            LinkedListReturn.add(pivot);
+
+            return LinkedListReturn;
+
+        }
+    }
 }
